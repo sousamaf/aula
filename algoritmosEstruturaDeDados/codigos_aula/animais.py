06690006fdda3f34f546106ddddafd2cbd15cde3
sousamaf <marco.volare@gmail.com>
2014-04-24 20:56:48 -0300

    Manipulação de arquivos
    Exemplo trabalhado em sala de aula.
    Manipulação de arquivos com a brincadeira dos animais.

---
+++ algoritmosEstruturaDeDados/codigos_aula/animais.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+arquivo = open('animais.txt')
+
+animais = []
+
+for animal in arquivo:
+	animais.append(animal[:-1].lower())
+arquivo.close()
+
+
+animal = input("Me diga o nome de um animal: ")
+animal = animal.lower()
+while True:
+	if animal == 'desisto':
+		print("pra nada")
+		break
+	if animal not in animais:
+		arquivo = open('animais.txt', 'a')
+		arquivo.write(animal)
+		arquivo.write('\n')
+		print("Legal, não conhecia esse animal.")
+		break
+	animal = input("Esse já conhecia, me diga outro: ")
+	animal = animal.lower()	
+
+
+arquivo.close()
