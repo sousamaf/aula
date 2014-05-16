2660987473766b985b43141ec80219305807f0fe
sousamaf <marco.volare@gmail.com>
2014-05-15 22:18:42 -0300

    ATIVIDADE 2
    Função de dividir os menores pelo maior.
    Função de validação de datas.

--- algoritmosEstruturaDeDados/codigos_aula/funcoes_inicio.py
+++ algoritmosEstruturaDeDados/codigos_aula/funcoes_inicio.py
@@ -19,9 +19,37 @@ def quadrado(numero):
 	quadro = numero ** 2
 	return quadro
 
+#Crie uma função que receba 3 números inteiros e retorna a soma dos dois menores divididos pelo maior.
+def menores_divididos_pelo_maior(numero1, numero2, numero3):
+	if numero1 > numero2 and numero1 > numero3:
+		return (numero2 + numero3) / numero1
+	if numero2 > numero1 and numero2 > numero3:
+		return (numero1 + numero3) / numero2
+	if numero3 > numero2 and numero3 > numero1:
+		return (numero1 + numero2) / numero3
+
+#Crie uma função que recebe dia, mês e ano e informa se é uma data válida.
+def data_valida(dia, mes, ano):
+	if (0 < dia < 32) and (0 < mes < 13) and (1499 < ano < 2100):
+		print("Data Válida")
+	else:
+		print("Data INVÁLIDA")
+
+
+
+
+
+
+
+
+
+
 imprimir_nome()
 idade(1989)
 dobro = dobro_de_pi()
 print(dobro)
 numero = quadrado(3)
 print(numero)
+b = menores_divididos_pelo_maior(3, 4, 5)
+print(b)
+data_valida(21, 12, 2014)
