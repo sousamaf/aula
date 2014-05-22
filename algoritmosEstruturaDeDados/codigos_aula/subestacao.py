1f8caaf153079e183d0db53d4fd4585735dc9e4e
sousamaf <marco.volare@gmail.com>
2014-05-22 20:25:58 -0300

    Localização de maior demanda
    Localização da maior demanda no arquivo de dados.

--- algoritmosEstruturaDeDados/codigos_aula/subestacao.py
+++ algoritmosEstruturaDeDados/codigos_aula/subestacao.py
@@ -2,11 +2,19 @@
 # -*- coding: utf-8 -*-
 
 def carregar_arquivo():
+	maior_demanda = 0
+	dia_hora_maior_demanda = " "
 	arquivo = open('subestacao.csv')
+	linha_1 = 0
 	for linha in arquivo:
-		dados = linha.split(',')
-		if dados[0] == '3/12/14' and dados[1] == '19:00:00000':
-			print(dados[2])
-
+		if linha_1 > 0:
+			dados = linha.split(',')
+			kwh = int(dados[2])
+			if kwh > maior_demanda:
+				maior_demanda = kwh
+				dia_hora_maior_demanda = dados[0] + " " +  dados[1]
+		else:
+			linha_1 = linha_1 + 1
+	print(dia_hora_maior_demanda)
 
 carregar_arquivo()
