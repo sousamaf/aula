d57e827d3fbd1be8e5165425776f3efc7a73900a
sousamaf <marco.volare@gmail.com>
2014-05-22 21:44:10 -0300

    criação da matriz m_dados
    criação da matriz global m_dados.

--- algoritmosEstruturaDeDados/codigos_aula/subestacao.py
+++ algoritmosEstruturaDeDados/codigos_aula/subestacao.py
@@ -1,20 +1,42 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+#m_dados = 
+
+'''
+A função carregar_arquivo varre todo o arquivo 
+subestacao.csv carregando seu conteúdo
+organizadamente para a matriz m_dados.
+'''
 def carregar_arquivo():
-	maior_demanda = 0
-	dia_hora_maior_demanda = " "
 	arquivo = open('subestacao.csv')
 	linha_1 = 0
 	for linha in arquivo:
 		if linha_1 > 0:
 			dados = linha.split(',')
-			kwh = int(dados[2])
-			if kwh > maior_demanda:
-				maior_demanda = kwh
-				dia_hora_maior_demanda = dados[0] + " " +  dados[1]
+			print(dados)
 		else:
 			linha_1 = linha_1 + 1
-	print(dia_hora_maior_demanda)
+
+
+'''
+A função demanda_maior localiza o dia e hora
+da maior demanda de um intervalo fechado fornecido.
+Caso não seja fornecido qualquer intervalo, todo
+o conteúdo carregado para a memória será o intervalo.
+É possível fornecer somente o dia, dia e hora ou
+nenhum intervalo.
+'''
+def demanda_maior(dia_inicio = "", hora_inicio = "", dia_fim = "", hora_fim = ""):
+
+'''
+A função demanda_media calcula a demanda média de
+um intervalo fornecido. Caso não seja informado um
+intervalo, todo o conteúdo em memória deve ser 
+considerado.
+'''
+def demanda_media(dia_inicio = "", hora_inicio = "", dia_fim = "", hora_fim = ""):
+
 
 carregar_arquivo()
+
