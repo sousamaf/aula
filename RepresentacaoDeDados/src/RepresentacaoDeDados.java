eaa4fe2dc0e4aa959063ecfd482ccb22d8c02ba7
Marco Sousa <marco.volare@gmail.com>
2018-05-08 09:42:46 -0300

    leitura do decimal
    código de leitura do valor decimal

--- RepresentacaoDeDados/src/RepresentacaoDeDados.java
+++ RepresentacaoDeDados/src/RepresentacaoDeDados.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
 
 public class RepresentacaoDeDados {
 	Integer decimal = (Integer) null;
 	int[] binario = new int[8];
 	int[] octal = new int[4];
 	int[] hexadecimal = new int[4];
+	Scanner entrada = new Scanner(System.in);	
+	
+	public void entradaDecimal()
+	{
+		System.out.print("Digite um valor decimal: ");
+		decimal = entrada.nextInt();
+	}
 	
 	public void menu() 
 	{
 		int opcao = 0;
+		
 		System.out.println("Representacao de Dados\n");
 		
 		if(decimal != null)
@@ -24,6 +33,27 @@ public class RepresentacaoDeDados {
 		System.out.println("\t2 para inserir octal.");
 		System.out.println("\t3 para inserir decimal.");
 		System.out.println("\t4 para inserir hexadecimal.");
+		
+		System.out.print("Entrada: ");
+		opcao = entrada.nextInt();
+		
+		switch (opcao) {
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			entradaDecimal();
+			break;
+		case 4:
+			
+			break;
+
+		default:
+			break;
+		}
 	}
 	
 	public static void main(String[] args) {
